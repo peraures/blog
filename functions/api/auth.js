2e2afebdfaa73b1d73d5051bb27b3f75c4a0eba9
PerAures <peraures@gmail.com>
2025-04-14 03:39:19 +0900

    add

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -1,28 +1,52 @@
 export async function onRequest(context) {
+    if (context.request.method !== "GET") {
+      return new Response("Method not allowed", { status: 405 });
+    }
+  
     const url = new URL(context.request.url);
     const code = url.searchParams.get("code");
+  
+    if (!code) {
+      return new Response("Missing code parameter", { status: 400 });
+    }
   
     const clientId = context.env.GITHUB_CLIENT_ID;
     const clientSecret = context.env.GITHUB_CLIENT_SECRET;
   
-    const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-      },
-      body: new URLSearchParams({
-        client_id: clientId,
-        client_secret: clientSecret,
-        code,
-      }),
-    });
+    try {
+      const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          client_id: clientId,
+          client_secret: clientSecret,
+          code,
+        }),
+      });
   
-    const tokenData = await tokenRes.json();
+      const tokenData = await tokenRes.json();
   
-    return new Response(JSON.stringify(tokenData), {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+      if (tokenData.error) {
+        console.log("GitHub token error:", tokenData);
+        return new Response(JSON.stringify(tokenData), {
+          status: 400,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      }
+  
+      return new Response(JSON.stringify(tokenData), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (err) {
+      console.error("Unexpected error during token exchange:", err);
+      return new Response("Internal Server Error", { status: 500 });
+    }
   }
   
